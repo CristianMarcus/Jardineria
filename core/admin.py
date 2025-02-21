d85576779d68cbca4d1cb400d047d379f2874345
MarcusCristian <cristianmarcus34@gmail.com>
2025-02-21 13:32:23 -0300

    Eliminar la aplicación 'core' y todos sus componentes relacionados

--- core/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Servicio, Cliente, Reserva
-
-# Registramos los modelos en el panel de administración
-admin.site.register(Servicio)
-admin.site.register(Cliente)
-admin.site.register(Reserva)
--- core/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('servicio/<int:pk>/', views.servicio_detalle, name='servicio_detalle'),
-    path('reserva/<int:pk>/', views.realizar_reserva, name='realizar_reserva'),
-]
